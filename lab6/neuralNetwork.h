dbcc99a3070770c9f00c89d36bf617f2a3fcf7e5
Nikhil Tom Jose <133224167+Ryuou02@users.noreply.github.com>
2024-09-26 06:56:23 +0530

    Update neuralNetwork.h

--- lab6/neuralNetwork.h
+++ lab6/neuralNetwork.h
@@ -1,6 +1,12 @@
 #pragma once
 #include"perceptron.h"
 #include<array>
+//#define SINGLE_OUTPUT
+#define SINGLE_HIDDEN
+
+
+#ifdef SINGLE_HIDDEN
+#ifdef SINGLE_OUTPUT
 class neuralNetwork
 {
 	int hiddenSize;		/* size of hidden layers */
@@ -69,6 +75,202 @@ public:
 				cout << loss;
 			}
 		}
+		std::cout << "\nthe final weights found for output layer is - \n";
+		for (int i = 0; i < 3; i++) {
+			std::cout << outputLayer[i] << '\n';
+		}
+		std::cout << "\n final weights for each perceptron - \n";
+		for (int i = 0; i < hiddenSize; i++) {
+			std::cout << "perceptron " << i << "\n";
+			for (int j = 0; j < 3; j++) {
+				std::cout << hiddenLayers[i][j] << '\n';
+			}
+		}
+	}
+};
+#endif
+#ifndef SINGLE_OUTPUT
+class neuralNetwork
+{
+	int hiddenSize;		/* size of hidden layers */
+	int outputLayerSize;/* number of outputs */
+	int n_inputs;		/* number of inputs */
+	perceptron* hiddenLayers, * outputLayers;
+	double* hiddenDel, * hiddenPrediction, *output, *outputPrediction, *outputDel;	/* del values for hidden layer and output layer neurons */
+public:
+	neuralNetwork(int hiddenLayerSize, int inputSize, int outputSize) :hiddenSize(hiddenLayerSize), n_inputs(inputSize), outputLayerSize(outputSize) {
+		hiddenLayers = new perceptron[hiddenLayerSize];
+		outputLayers = new perceptron[outputLayerSize];
+		for (int i = 0; i < hiddenSize; i++) {
+			hiddenLayers[i] = perceptron(tan_h, inputSize);
+		}
+		for (int i = 0; i < outputLayerSize; i++) {
+			outputLayers[i] = perceptron(tan_h, hiddenLayerSize);
+		}
+		/* below variables are only used for training/back propogation but re-allocation of memory will take lot of time, hence making it a class variable*/
+		
+		hiddenPrediction = new double[hiddenSize];
+		hiddenDel = new double[hiddenSize];
+		outputPrediction = new double[outputLayerSize];
+		outputDel = new double[outputLayerSize];
+	}
+	~neuralNetwork() {
+		delete[] hiddenLayers;
+		delete[] hiddenPrediction;
+		delete[] hiddenDel;
+		delete[] outputDel;
+		delete[] outputPrediction;
+	}
+	double* predict(double* inputs) {
+		double* pred = new double[outputLayerSize];
+		for (int i = 0; i < hiddenSize; i++)
+			hiddenPrediction[i] = hiddenLayers[i].predict(inputs);
+		for (int i = 0; i < outputLayerSize; i++)
+			 pred[i] = outputLayers[i].predict(hiddenPrediction);
+
+		return pred;
+	}
+	void backPropogate(double* input, double* requiredOutput, float learningRate) {
+		/* in the back propogation the predict function is not used directly, but the code for it is used in it. */
+		
+		for (int i = 0; i < hiddenSize; i++)
+			hiddenPrediction[i] = hiddenLayers[i].predict(input);
+
+		for (int i = 0; i < outputLayerSize; i++)
+			outputPrediction[i] = outputLayers[i].predict(hiddenPrediction);
+
+		// calculating del values for every neuron, in order to update the weights.
+		for (int i = 0; i < outputLayerSize; i++) {
+			outputDel[i] =  outputPrediction[i] * (1 - outputPrediction[i]) * (requiredOutput[i] - outputPrediction[i]);
+			outputLayers[i].setWeights(0, outputLayers[i][0] + learningRate * outputDel[i]);
+			for (int j = 0; j < hiddenSize; j++)
+				outputLayers[i].setWeights(j + 1, outputLayers[i][j + 1] + learningRate* hiddenPrediction[j] * outputDel[i]);
+		}
+
+		for (int i = 0; i < hiddenSize; i++) {
+			hiddenDel[i] = 0;
+			for (int j = 0; j < outputLayerSize; j++)
+				hiddenDel[i] +=  hiddenPrediction[i] * (1 - hiddenPrediction[i]) * (outputLayers[j][i + 1] * outputDel[j]);	// outputLayer[i] => ith weight of output layer
+			//setting new weights for hidden layers
+			hiddenLayers[i].setWeights(0, hiddenLayers[i][0] + learningRate * hiddenDel[i]);
+			for (int j = 0; j < n_inputs; j++)
+				hiddenLayers[i].setWeights(j + 1, hiddenLayers[i][j + 1] + learningRate * hiddenDel[i] * input[j]);
+		}
+	}
+	void train(vector<double*> inputs, vector<double*> outputs, int nEpochs, float learningRate = 1) {
+		for (int epoch = 0; epoch < nEpochs; epoch++) {
+			for (int i = 0; i < inputs.size(); i++) {
+				backPropogate(inputs[i], outputs[i], learningRate);
+			}
+			//std::cout << "\n" << epoch <<;
+			if (epoch % 500 == 0) {
+				cout << "\nat epoch " << epoch << ", loss : ";
+				double loss = 0;
+				for (int i = 0; i < 4; i++) {
+					double* pred = predict(inputs[i]);
+					for (int j = 0; j < outputLayerSize; j++) {
+						loss += pow(pred[j] - outputs[i][j], 2);
+					}
+
+				}
+				loss /= 4;
+				cout << loss;
+
+			}
+		}
+	}
+};
+#endif
+#endif
+#ifndef SINGLE_HIDDEN
+class neuralNetwork
+{
+	int *hiddenSize;	/* size of  each of the hidden layers */
+	int outputLayerSize;/* number of outputs */
+	int n_inputs;		/* number of inputs */
+	perceptron** hiddenLayers, * outputLayers;
+	double** hiddenDel, ** hiddenPrediction, * output, * outputPrediction, * outputDel;	/* del values for hidden layer and output layer neurons */
+public:
+	neuralNetwork(int hiddenLayerSize, int inputSize, int outputSize) :hiddenSize(hiddenLayerSize), n_inputs(inputSize), outputLayerSize(outputSize) {
+		hiddenLayers = new perceptron[hiddenLayerSize];
+		outputLayers = new perceptron[outputLayerSize];
+		for (int i = 0; i < hiddenSize; i++) {
+			hiddenLayers[i] = perceptron(sigmoid, inputSize);
+		}
+		for (int i = 0; i < outputLayerSize; i++) {
+			outputLayers[i] = perceptron(sigmoid, hiddenLayerSize);
+		}
+		/* below variables are only used for training/back propogation but re-allocation of memory will take lot of time, hence making it a class variable*/
+
+		hiddenPrediction = new double[hiddenSize];
+		hiddenDel = new double[hiddenSize];
+		outputPrediction = new double[outputLayerSize];
+		outputDel = new double[outputLayerSize];
+	}
+	~neuralNetwork() {
+		delete[] hiddenLayers;
+		delete[] hiddenPrediction;
+		delete[] hiddenDel;
+		delete[] outputDel;
+		delete[] outputPrediction;
+	}
+	double* predict(double* inputs) {
+		double* pred = new double[outputLayerSize];
+		for (int i = 0; i < hiddenSize; i++)
+			hiddenPrediction[i] = hiddenLayers[i].predict(inputs);
+		for (int i = 0; i < outputLayerSize; i++)
+			pred[i] = outputLayers[i].predict(hiddenPrediction);
+
+		return pred;
+	}
+	void backPropogate(double* input, double* requiredOutput, float learningRate) {
+		/* in the back propogation the predict function is not used directly, but the code for it is used in it. */
+
+		for (int i = 0; i < hiddenSize; i++)
+			hiddenPrediction[i] = hiddenLayers[i].predict(input);
+
+		for (int i = 0; i < outputLayerSize; i++)
+			outputPrediction[i] = outputLayers[i].predict(hiddenPrediction);
+
+		// calculating del values for every neuron, in order to update the weights.
+		for (int i = 0; i < outputLayerSize; i++) {
+			outputDel[i] = outputPrediction[i] * (1 - outputPrediction[i]) * (requiredOutput[i] - outputPrediction[i]);
+			outputLayers[i].setWeights(0, outputLayers[i][0] + learningRate * outputDel[i]);
+			for (int j = 0; j < hiddenSize; j++)
+				outputLayers[i].setWeights(j + 1, outputLayers[i][j + 1] + learningRate * hiddenPrediction[j] * outputDel[i]);
+		}
+
+		for (int i = 0; i < hiddenSize; i++) {
+			hiddenDel[i] = 0;
+			for (int j = 0; j < outputLayerSize; j++)
+				hiddenDel[i] += hiddenPrediction[i] * (1 - hiddenPrediction[i]) * (outputLayers[j][i + 1] * outputDel[j]);	// outputLayer[i] => ith weight of output layer
+			//setting new weights for hidden layers
+			hiddenLayers[i].setWeights(0, hiddenLayers[i][0] + learningRate * hiddenDel[i]);
+			for (int j = 0; j < n_inputs; j++)
+				hiddenLayers[i].setWeights(j + 1, hiddenLayers[i][j + 1] + learningRate * hiddenDel[i] * input[j]);
+		}
+	}
+	void train(vector<double*> inputs, vector<double*> outputs, int nEpochs, float learningRate = 1) {
+		for (int epoch = 0; epoch < nEpochs; epoch++) {
+			for (int i = 0; i < inputs.size(); i++) {
+				backPropogate(inputs[i], outputs[i], learningRate);
+			}
+			if (epoch % 500 == 0) {
+				cout << "\nat epoch " << epoch << ", loss : ";
+				double loss = 0;
+				for (int i = 0; i < 4; i++) {
+					double* pred = predict(inputs[i]);
+					for (int j = 0; j < outputLayerSize; j++) {
+						loss += pow(pred[j] - outputs[i][j], 2);
+					}
+
+				}
+				loss /= 4;
+				cout << loss;
+
+			}
+		}
 	}
 };
 
+#endif
